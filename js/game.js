16efb71f5fe30a7afbdeb9f25b2a41e82863cc07
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-05-08 20:01:38 +0000

    added background, game.js and modified index.html and script.js

---
+++ js/game.js
@@ -0,0 +1,11 @@
+/* global Phaser */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 4092545
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
