dd69a73b8b9761842c2f00d6dc360ccf306b3519
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-05-08 20:25:32 +0000

    added splashScene.js and modified index.html and game.js

--- js/game.js
+++ js/game.js
@@ -1,11 +1,39 @@
-/* global Phaser */
+/* Global Phaser */
 
+// Scene import statements
+import SplashScene from "./splashScene.js"
+
+// Create the new scenes
+const splashScene = new SplashScene()
+
+// Start Phaser Game
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 4092545
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  
+  // set background colour
+  backgroundColor: 4092545,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    
+    // We place the Game in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
-console.log(game)
+// console.log(game)
+
+// Load scenes
+// Note: remember any "key" is global and CAN NOT be reused
+game.scene.add("splashScene", splashScene)
+
+// Start the game
+game.scene.start("splashScene")
