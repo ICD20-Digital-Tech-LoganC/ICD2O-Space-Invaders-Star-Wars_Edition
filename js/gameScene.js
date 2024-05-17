e07ef511fe5518b72a497a6632783bd2a89be5ab
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-05-17 14:52:31 +0000

    fixed movement using velocity physics

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,75 +1,90 @@
+
+
 class GameScene extends Phaser.Scene {
 
-  constructor () {
-    
+  constructor() {
+
     super({ key: 'gameScene' })
     this.gameSceneBackgroundImage = null
     this.player = null
     this.firelaser = false
-    
+
   }
 
-  init (data) {
-    
+  init(data) {
+
     this.cameras.main.setBackgroundColor('#ffffff')
-    
+
   }
 
-  preload () {
-    
+  preload() {
+
     console.log('gameScene')
 
-    // images
+    // loads images
     this.load.image('gameSceneBackground', './images/space-background.jpg')
     this.load.image('player', './images/arc170_starfighter.png')
     this.load.image('laser', './images/laser.jpg')
-    
+
   }
 
-  create (data) {
+  create(data) {
 
+    
     this.gameSceneBackgroundImage = this.add.image(0, 0, 'gameSceneBackground')
     this.gameSceneBackgroundImage.setOrigin(0, 0)
-
+    
+    // adds player phyics
     this.player = this.physics.add.sprite(1920 / 2, 1080 - 100, 'player')
 
     // create a group for the lasers
     this.laserGroup = this.physics.add.group()
-    
+
   }
 
-  update (time, delta) {
-    
+  update(time, delta) {
+
     // player movement
     const keyLeftObj = this.input.keyboard.addKey('A')
     const keyRightObj = this.input.keyboard.addKey('D')
+    const keyDownObj = this.input.keyboard.addKey('S')
+    const keyUpObj = this.input.keyboard.addKey('W')
     const keySpaceObj = this.input.keyboard.addKey('SPACE')
 
+    // set player velocity to zero by default
+    this.player.setVelocity(0);
+
+    // move left if a is pressed
     if (keyLeftObj.isDown === true) {
-      this.player.x -= 15
-      if (this.player.x < 0) {
-        this.player.x = 0
-      }
+      this.player.setVelocityX(-300)
+    }
 
-      if (keyRightObj.isDown === true) {
-        this.player.x += 15
-        if (this.player.x > 1920) {
-          this.player.x = 1920
-        }
-      }
+    // move right if d is pressed
+    if (keyRightObj.isDown === true) {
+      this.player.setVelocityX(300);
+    }
 
-      if (keySpaceObj.isDown === true) {
-        if (this.firelaser === false) {
-          
-          // fire laser
-          this.firelaser = true
-          const newLaser = this.physics.add.sprite(this.player.x, this.player.y, 'laser')
-        }
-      }
+    // move down if s is pressed
+    if (keyDownObj.isDown === true) {
+      this.player.setVelocityY(300);
+    }
 
-      if (keySpaceObj.isUp === true) {
-        this.firelaser = false
+    // move up if w is pressed
+    if (keyUpObj.isDown === true) {
+      this.player.setVelocityY(-300);
+    }
+    
+    // fire laser using spacebar if the spacebar is not released or being pressed
+    if (keySpaceObj.isDown === true) {
+      if (this.firelaser === false) {
+        this.firelaser = true
+        const newLaser = this.physics.add.sprite(this.player.x, this.player.y, 'laser')
       }
+    }
+
+    // if the spacebar is released or not being pressed
+    if (keySpaceObj.isUp === true) {
+      this.firelaser = false
     }
   }
 }
